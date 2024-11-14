53b35d96dd2745105c2aa76a005b0ddf14b40fcb
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-11-14 14:33:01 +0100

    Update calculateRevisitTime.m

--- Prosjekt3/Matlab/calculateRevisitTime.m
+++ Prosjekt3/Matlab/calculateRevisitTime.m
@@ -2,9 +2,15 @@ function calculateRevisitTime(access)
     intervals = accessIntervals(access);
     startTimes = intervals.StartTime; % Hent starttidspunktene
     startTimesNum = datenum(startTimes); % Konverter til numeriske verdier
-    revisitTimes = diff(startTimesNum); % Beregn forskjellene
-    avgRevisitTime = mean(revisitTimes) * 24 * 60; % Konverter fra dager til minutter
-    maxRevisitTime = max(revisitTimes) * 24 * 60; % Konverter fra dager til minutter
-    fprintf('Gjennomsnittlig revisit-tid: %.2f minutter\n', avgRevisitTime);
-    fprintf('Maksimal revisit-tid: %.2f minutter\n', maxRevisitTime);
+    revisitTimes = diff(startTimesNum) * 24 * 60; % Beregn forskjellene og konverter til minutter
+    
+    avgRevisitTime = mean(revisitTimes); % Gjennomsnittlig revisit-tid i minutter
+    maxRevisitTime = max(revisitTimes); % Maksimal revisit-tid i minutter
+    
+    % Konverter til HH:MM:SS format
+    avgRevisitDuration = duration(0, avgRevisitTime, 0, 'Format', 'hh:mm:ss');
+    maxRevisitDuration = duration(0, maxRevisitTime, 0, 'Format', 'hh:mm:ss');
+    
+    fprintf('Gjennomsnittlig revisit-tid: %s\n', avgRevisitDuration);
+    fprintf('Maksimal revisit-tid: %s\n', maxRevisitDuration);
 end
